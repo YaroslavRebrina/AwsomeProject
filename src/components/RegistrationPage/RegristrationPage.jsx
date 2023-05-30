c4df55694333ce4b63a62304242dcc51bebd7002
Yaroslav Rebrina <yaroslavrebrina@gmail.com>
2023-05-30 18:17:45 +0300

    feat: reg. page added inputs, button

--- src/components/RegistrationPage/RegristrationPage.jsx
+++ src/components/RegistrationPage/RegristrationPage.jsx
@@ -1,21 +1,18 @@
-import {
-  Image,
-  ImageBackground,
-  StyleSheet,
-  Text,
-  TextInput,
-  View,
-} from "react-native";
+import { Button, Pressable, StyleSheet, Text, TextInput } from "react-native";
+import colors from "../../constatns/colors/colors";
 
 export const RegistrationPage = () => {
   return (
     <>
       {/* <ImageBackground source={AuthBg} style={styles.image}> */}
       <Text>Реєстрація</Text>
-      <TextInput style={styles.input}></TextInput>
-      <TextInput style={styles.input}></TextInput>
-      <TextInput style={styles.input}></TextInput>
+      <TextInput style={styles.input} />
+      <TextInput style={styles.input} />
+      <TextInput style={styles.input} />
       {/* </ImageBackground> */}
+      <Pressable style={styles.button}>
+        <Text style={styles.text}>Зареєструватися</Text>
+      </Pressable>
     </>
   );
 };
@@ -23,9 +20,37 @@ export const RegistrationPage = () => {
 const styles = StyleSheet.create({
   input: {
     height: 50,
-    width: "95%",
+    width: "100%",
     backgroundColor: "#F6F6F6",
     borderWidth: 1,
     borderRadius: 8,
   },
+
+  button: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    borderRadius: 50,
+    backgroundColor: colors.orangeMain,
+    paddingVertical: 16,
+    width: "100%",
+  },
+
+  text: {
+    fontFamily: "Roboto",
+    fontStyle: "normal",
+    fontWeight: 400,
+    fontSize: 16,
+    lineHeight: 19,
+    color: colors.white,
+  },
+  //   text: {
+  //     font-family: 'Roboto';
+  // font-style: normal;
+  // font-weight: 400;
+  // font-size: 16px;
+  // line-height: 19px;
+  // /* identical to box height */
+
+  // text-align: center;
 });
---
+++ src/constatns/colors/colors.js
@@ -0,0 +1,9 @@
+const orangeMain = "#FF6C00";
+const white = "#FFFFFF";
+
+const colors = {
+  orangeMain,
+  white,
+};
+
+export default colors;
